ffb8f6fabb16f4566b893eeb105836fdad776ac6
MahdiZare64 <mzare0930@gmail.com>
2024-11-20 23:23:52 +0330

    rename component

--- src/pages/AnalyticChart.tsx
+++ src/pages/AnalyticChart.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import FullChart from "../components/FullChart";
+import OLHCVChart from "../components/OLHCVChart";
 
 function Analytic() {
   return (
     <main className="w-full h-full min-h-screen d-block bg-gray-50">
       <div className="w-100 flex pt-8 px-4">
         <div className="w-7/12">
-          <FullChart />
+          <OLHCVChart />
         </div>
       </div>
     </main>
